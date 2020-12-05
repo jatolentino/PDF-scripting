ed8f9efe2f257c6dfdd21334fe0c95a8438299a8
josetv91 <61167951+josetv91@users.noreply.github.com>
2020-12-05 02:50:50 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,7 +1,9 @@
 for (var p=0; p<this.numPages; p++) {
+    
     var Recta = this.addAnnot({type: "Square", page: p, rect: [29,48,567,80], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
     var Recta1 = this.addAnnot({type: "Square", page: p, rect: [29,48,467,80], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
     Recta1.fillColor = color.white;
-var Recta2 = this.addAnnot({type: "Square", page: p, rect: [467,65,490,80], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
-Recta2.fillColor = color.white;
+    
+    var Recta2 = this.addAnnot({type: "Square", page: p, rect: [467,65,490,80], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
+    Recta2.fillColor = color.white;
 }
