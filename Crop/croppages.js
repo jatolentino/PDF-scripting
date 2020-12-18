d6c078ecae3ac4adc9d102e7a33b0f556d989191
josetv91 <61167951+josetv91@users.noreply.github.com>
2020-12-18 00:16:53 -0500

    Update croppages.js

--- Crop/croppages.js
+++ Crop/croppages.js
@@ -1,10 +1,10 @@
 for (var p=0; p<this.numPages; p++) {
     
   var rCrop = this.getPageBox("Crop",p); 
-  rCrop[0] += 70; // Adjust Left Side 
-  rCrop[1] -= 50; // Adjust Top Side 
-  rCrop[2] -= 200; // Adjust Right Side 
-  rCrop[3] += 50; // Adjust Bottom Side 
+  rCrop[0] += 70; // Adjust Left Side 85
+  rCrop[1] -= 50; // Adjust Top Side 69
+  rCrop[2] -= 200; // Adjust Right Side 224 
+  rCrop[3] += 50; // Adjust Bottom Side 70
   this.setPageBoxes("Crop",p,p,rCrop);
     
 }
