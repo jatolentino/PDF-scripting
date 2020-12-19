35f522648749264055e8429c68b0e81ec0d89e5a
josetv91 <61167951+josetv91@users.noreply.github.com>
2020-12-19 16:51:26 -0500

    Update form.js

--- form.js
+++ form.js
@@ -6,4 +6,7 @@ for (var p=0; p<this.numPages; p++) {
     
     var Recta2 = this.addAnnot({type: "Square", page: p, rect: [467,65,490,80], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
     Recta2.fillColor = color.white;
+    
+    // Command to get the size of the page i.e. 3
+    //this.getPageBox("Crop", 3)
 }
