b57075099cfa6c5af4c190169de67ca3a8327c0c
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-05-13 23:49:20 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,8 @@
+for (var p=0; p<this.numPages; p++) {
+    
+    var Recta = this.addAnnot({type: "Square", page: p, rect: [-1,787,596.5,843], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
+    Recta.fillColor = color.white;
+		var Recta1 = this.addAnnot({type: "Square", page: p, rect: [-1,0,596.5,18.5], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
+    Recta1.fillColor = color.white;
+    
+}
