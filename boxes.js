5377ed78ad5b91cbe725374d6db445b813f07abf
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-05-13 23:49:38 -0500

    Update and rename .js to boxes.js

--- boxes.js
+++ boxes.js
@@ -4,5 +4,8 @@ for (var p=0; p<this.numPages; p++) {
     Recta.fillColor = color.white;
 		var Recta1 = this.addAnnot({type: "Square", page: p, rect: [-1,0,596.5,18.5], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
     Recta1.fillColor = color.white;
-    
+    var Recta2 = this.addAnnot({type: "Square", page: p, rect: [-1,0,30,843], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
+    Recta2.fillColor = color.white;
+    var Recta3 = this.addAnnot({type: "Square", page: p, rect: [543,0,596.5,843], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
+    Recta3.fillColor = color.white;
 }
