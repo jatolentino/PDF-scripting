c977c6181fa317a5d6ed932532bb62f685ade4aa
Jose A. Tolentino <61167951+josetv91@users.noreply.github.com>
2021-11-06 21:42:02 -0500

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -9,4 +9,3 @@ for (var p=0; p<this.numPages; p++) {
     var Recta3 = this.addAnnot({type: "Square", page: p, rect: [543,0,596.5,843], width:2, strokeColor: color.white, opacity: 1, readOnly: true});
     Recta3.fillColor = color.white;
 }
-
